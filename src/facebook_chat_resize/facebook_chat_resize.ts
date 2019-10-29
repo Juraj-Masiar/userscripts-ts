a51373aac70ad9a9273064ec9310ac4c46a3beca
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-10-29 16:23:33 +0100

    resizing done, more less...

--- src/facebook_chat_resize/facebook_chat_resize.ts
+++ src/facebook_chat_resize/facebook_chat_resize.ts
@@ -3,10 +3,27 @@ import {DynamicStyle} from '../utils/dynamic_style';
 import {timeoutPromise} from '../utils/utils_module';
 
 
-
 DynamicStyle.loadCSS(`
 .opened {
   width: 400px !important;
 }
 `);
+
+windowLoadedPromise.then(async () => {
+    await timeoutPromise(1000);
+
+    document.querySelectorAll<HTMLElement>('div.titlebar')
+        .forEach(titleNode => {
+            titleNode.onclick = async () => {
+                console.log('user clicked');
+                await timeoutPromise(1000);
+                document.querySelectorAll<HTMLElement>('div.titlebar')
+                    .forEach(titleNode => {
+                        const body = titleNode.nextElementSibling as HTMLElement;
+                        body.style.height = '500px';
+
+                    })
+            }
+        })
+})
 
